4a985f873ea8e1dd43e4c1b55a3d8b85d7b55b1d
Reid <touyalin@gmail.com>
2023-07-01 18:05:53 +0800

    5.38 create more components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import moment from "moment";
 
-function App() {
-  return <h1>Hello React!</h1>;
-}
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Hello React!</h1>
+      <Menu />
+      <Footer />
+    </div>
+  );
+};
+
+const Header = () => {
+  return <h1>Fast React Pizza Co.</h1>;
+};
+
+const Menu = () => {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
+    </div>
+  );
+};
+
+const Footer = () => {
+  const now = moment().format("MMM Do YYYY");
+  return <footer>{now} We're currently open </footer>;
+};
+
+const Pizza = () => {
+  return (
+    <div>
+      <img alt="Pizza spinaci" src="./pizzas/spinaci.jpg" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
