1a3e25c99f788d28a68b1de21d60683c1acbce06
Reid <touyalin@gmail.com>
2023-07-01 20:37:42 +0800

    5.52 destructuring props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import moment from "moment";
+import "./index.css";
+import pizzaData from "./data.js";
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <Header />
-      <h1>Hello React!</h1>
       <Menu />
       <Footer />
     </div>
@@ -14,29 +15,76 @@ const App = () => {
 };
 
 const Header = () => {
-  return <h1>Fast React Pizza Co.</h1>;
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
 };
 
 const Menu = () => {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
-    <div>
+    <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-    </div>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
+    </main>
+  );
+};
+
+const Pizza = ({ pizzaObj }) => {
+  const { name, photoName, ingredients, price } = pizzaObj;
+
+  return (
+    <li className="pizza">
+      <img alt={name} src={photoName} />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{parseFloat(price, 10)}</span>
+      </div>
+    </li>
   );
 };
 
 const Footer = () => {
-  const now = moment().format("MMM Do YYYY");
-  return <footer>{now} We're currently open </footer>;
+  // const now = moment().format("MMM Do YYYY");
+  const currentHour = moment().hour();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = currentHour >= openHour && currentHour <= closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
+    </footer>
+  );
 };
 
-const Pizza = () => {
+const Order = ({ closeHour, openHour }) => {
   return (
-    <div>
-      <img alt="Pizza spinaci" src="./pizzas/spinaci.jpg" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    <div className="order">
+      <p>
+        We're currently open from {openHour}:00 to {closeHour}:00. Come visit us
+        or order online.
+      </p>
+      <button className="btn">Order</button>
     </div>
   );
 };
